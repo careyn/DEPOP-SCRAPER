967c68dac5fa657a7bf6fee2eacc624da8cda416
careyn <carey.n@northeastern.edu>
2021-05-16 12:12:02 +0100

    added URL parser

--- depopScraper.py
+++ depopScraper.py
@@ -3,29 +3,61 @@ from selenium import webdriver
 import time
 
 # url of the page we want to scrape
-url = input("Enter URL:")
+from selenium.webdriver.common.keys import Keys
 
-# initiating the webdriver. Parameter includes the path of the webdriver.
+home = input("Enter URL:")
+
 driver = webdriver.Chrome('./chromedriver')
-driver.get(url)
-
-# this is just to ensure that the page is loaded
+driver.get(home)
+# ensure the page loads image source (dynamic property)
 time.sleep(3)
+
+elem = driver.find_element_by_tag_name("body")
+
+no_of_pagedowns = 20
+
+while no_of_pagedowns:
+    elem.send_keys(Keys.PAGE_DOWN)
+    time.sleep(0.2)
+    no_of_pagedowns-=1
 
 html = driver.page_source
 
 soup = BeautifulSoup(html, "html.parser")
 
-descriptionHtml = soup.find(class_="Text-yok90d-0 styles__DescriptionContainer-uwktmu-9 bWcgji")
+urls = soup.find_all("a", {"data-testid" : "product__item"})
 
-print(descriptionHtml.contents[0])
+def scrapeData(url):
+    # Open the chrome web driver
+    driver = webdriver.Chrome('./chromedriver')
+    driver.get(url)
 
-priceHtml = soup.find("span", {"data-testid" : "fullPrice"})
+    # ensure the page loads image source (dynamic property)
+    time.sleep(1)
 
-print(priceHtml.contents[0])
+    html = driver.page_source
 
-img = soup.find("img", {"class" : "styles__Image-uwktmu-7 cKdjfY LazyLoadImage__Image-sc-1732jps-1 cSwkPp"})
+    soup = BeautifulSoup(html, "html.parser")
 
-print(img.get("src"))
+    descriptionHtml = soup.find(class_="Text-yok90d-0 styles__DescriptionContainer-uwktmu-9 bWcgji")
+
+    desc = descriptionHtml.contents[0]
+
+    priceHtml = soup.find("span", {"data-testid" : "fullPrice"})
+
+    price = priceHtml.contents[0]
+
+    img = soup.find("img", {"class" : "styles__Image-uwktmu-7 cKdjfY LazyLoadImage__Image-sc-1732jps-1 cSwkPp"})
+
+    src = img.get("src")
+
+    # close the web driver
+    driver.close()
+
+    return [price, src, desc]
+
+
+for i in urls:
+    print(scrapeData("https://www.depop.com" + i.get("href")))
 
 driver.close()
